bab3e627aae955a8e645122ec21201f25faef214
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-28 18:42:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,9 +5,10 @@ def hello_t(array)
     yield array[i]
     i += 1
   end
-
-
 end
 
-# call your method here!
-
+hello_t(["Tim", "Tom", "Harry"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
